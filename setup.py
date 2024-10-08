6e2a2dfee2cc32f161a1dc0b06ef5cea90699eee
Yasmine Nahal <72565158+yasminenahal@users.noreply.github.com>
2024-10-08 19:28:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="hitl_al_gomg",
-    version="0.0.23",
+    version="0.0.25",
     license="MIT",
     author="Yasmine Nahal",
     author_email="yasmine.nahal@aalto.fi",
